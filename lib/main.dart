f77e8dc7aa1380e384f0df8fd7bed32c82762d22
Ayushi Chaudhuri <61285140+IUC4801@users.noreply.github.com>
2022-04-23 22:48:20 +0530

    added new files

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:clicky/constants.dart';
+import 'package:clicky/views/screens/auth/login_screen.dart';
+import 'package:clicky/views/screens/auth/signup_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Clicky',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+      home: SignupScreen(),
+    );
+  }
+}
